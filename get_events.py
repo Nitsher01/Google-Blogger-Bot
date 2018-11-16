ccfb92969e27d8be537182eae2022d0f78061bef
Nitin Sharma <nitinsharmas34@gmail.com>
2018-11-16 14:10:46 +0530

    Update get_events.py

--- get_events.py
+++ get_events.py
@@ -4,6 +4,15 @@ import datetime
 
 def get_daily_events():
 	events = dict()
+	today = datetime.date.today()
+	day = today.strftime('%d')
+	month = today.strftime('%m')
+	for i in range(500):
+		try:
 			with req.urlopen(('http://numbersapi.com/')+(month)+('/')+(day)+'/date?json') as url:
+				data = json.loads(url.read().decode())
+				events[int(data['year'])] = data['text']
+				time.sleep(1)
+		except Exception:
 			print('Exception occurred ')
 	return events
